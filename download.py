8881be54864f1e287887a243b70985f979b174cb
William Merrill <vikingarnir.will@gmail.com>
2017-04-01 23:56:17 -0400

    stopped using dota2api

--- download.py
+++ download.py
@@ -1,47 +1,10 @@
-import dota2api, pickle, time
+import urllib3
 
-KEY = "B2666587DD228A597DF0A3B8FC25FCF6"
+http = urllib3.PoolManager()
 
-api = dota2api.Initialise(KEY)
+def get(url):
+	r = http.request("GET", url)
+	return r.data
 
-# leagueListing = api.get_league_listing()
-# print len(leagueListing["leagues"])
-# print leagueListing["leagues"][0]
-
-def downloadMatches(nextMatch, n, matchType):
-
-	allMatches = []
-	for i in range (n / 100):
-		try:
-			matchHistory = api.get_match_history_by_seq_num(start_at_match_seq_num=nextMatch, **matchType)
-			nextMatch = matchHistory["matches"][99]["match_seq_num"] + 1
-			allMatches += matchHistory["matches"]
-		except ValueError:
-			print "[WAIT] max API requests exceeded"
-			time.sleep(60)
-			break
-
-	return allMatches, nextMatch
-
-# start of 7.0.0 -- match_id: 2841970697
-
-GAMES = 10000
-FILENAME = "matches-" + str(GAMES) + ".data"
-TYPE = {
-	"game_mode": 2, #captain's mode
-	"lobby_type": 2, #tournament
-}
-
-if __name__ == "__main__":
-
-	t = api.get_match_details(2841970697)["match_seq_num"]
-	startT = t
-	allMatches = []
-	while len(allMatches) < GAMES:
-		print "got", len(allMatches), "/", GAMES, "matches"
-		matches, t = downloadMatches(t,100, TYPE)
-		allMatches += matches
-
-	print "last match:", t
-	print "saving results to " + FILENAME + ".."
-	pickle.dump(allMatches, open(FILENAME, "wb"))
+print get("http://api.opendota.com/api/proMatches")
+# print get("https://api.opendota.com/api/explorer?sql=select%20picks_bans,radiant_win%20from%20matches%20order%20by%20match_id%20desc%20limit%20100%20offset%20100")
