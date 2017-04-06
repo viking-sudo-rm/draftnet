285d819990c3eef03937baa516cf12521470ef68
dmontealegre <daniel.montealegre@yale.edu>
2017-04-06 17:28:25 -0400

    feature vectors

--- download.py
+++ download.py
@@ -3,39 +3,41 @@ import requests, pickle, time, random
 N = 100000
 WAIT_TIME = 60
 
+
 def get(url):
-	return requests.get(url).json()
+    return requests.get(url).json()
+
 
 matches = []
 
 for i in range(0, N, 100):
-	res = get("https://api.opendota.com/api/explorer?sql=select%20match_id,picks_bans,radiant_win%20from%20matches%20where%20game_mode=2%20and%20picks_bans%20is%20not%20null%20order%20by%20match_id%20desc%20limit%20100%20offset%20" + str(i))
-	
-	if "error" in res.keys():
-		print "waiting", WAIT_TIME, "sec for API.."
-		time.sleep(WAIT_TIME)
-		continue
+    res = get("https://api.opendota.com/api/explorer?sql=select%20match_id,picks_bans,radiant_win%20from%20matches%20where%20game_mode=2%20and%20picks_bans%20is%20not%20null%20order%20by%20match_id%20desc%20limit%20100%20offset%20" + str(i))
 
-	matches += res["rows"]
-	print "got", i + 100, "/", N, "matches"
+    if "error" in res.keys():
+        print("waiting", WAIT_TIME, "sec for API..")
+        time.sleep(WAIT_TIME)
+        continue
 
-print "checking downloaded game data.."
-print "# with drafts:", sum(map(lambda x: 0 if x["picks_bans"] == None else 1, matches))
-print "# total:", len(matches)
+    matches += res["rows"]
+    print("got", i + 100, "/", N, "matches")
 
-print "saving match data.."
+print("checking downloaded game data..")
+print("# with drafts:", sum(map(lambda x: 0 if x["picks_bans"] == None else 1, matches)))
+print("# total:", len(matches))
+
+print("saving match data..")
 filename = "matches-" + len(matches) + ".data"
 pickle.dump(matches, open(filename, "wb"))
 
-print "sampling test and train datasets.."
+print("sampling test and train datasets..")
 random.shuffle(matches)
-test = matches[:N / 10]
-train = matches[N / 10:]
+test = matches[:N // 10]
+train = matches[N // 10:]
 
-print "saving test dataset.."
+print("saving test dataset..")
 filename = "test-" + len(test) + ".data"
 pickle.dump(test, open(filename, "wb"))
 
-print "saving train dataset.."
+print("saving train dataset..")
 filename = "train-" + len(train) + ".data"
 pickle.dump(train, open(filename, "wb"))
--- train.py
+++ train.py
@@ -1,104 +1,140 @@
+from __future__ import print_function
 import pickle, random
 import tensorflow as tf
 import numpy as np
 from display import *
 
+
 N = 113
-M = 2 * N
+M = N*10
 L = 5 + N
 
 BATCH_SIZE = 10
 LEARNING_RATE = 0.5
 
+# the following three functions are to construct our vectors from the file.
 flatten = lambda l: [item for sublist in l for item in sublist]
 
+ROLES = ['Support', 'Jungler', 'Escape', 'Carry', 'Durable', 'Nuker', 'Pusher', 'Disabler', 'Initiator']
+
+# pick is a dict with keys: hero, team, order
 def getOneHot(pick):
-	return [1 if i == pick["hero_id"] - 1 else 0 for i in range(N)]
+    return [1 if i == getShiftedID(pick["hero_id"]) else 0 for i in range(N)]
 
-def getData(filename, winOnly = True):
-	raw = pickle.load(open(filename, "rb"))
-	won = lambda game: (game["picks_bans"][-1]["team"] == 0 and game["radiant_win"]) or (game["picks_bans"][-1]["team"] == 1 and not game["radiant_win"])
-	X = [(flatten(map(getOneHot, game["picks_bans"][:-1])) + [game["picks_bans"][0]["team"]] + [] if winOnly else [won(game)], getOneHot(game["picks_bans"][-1])) for game in raw if len(game["picks_bans"]) == 20 and (not winOnly or won(game))]
-	return X
+def getFeatures(pick):
+    features = []
+    hero = heroes[getShiftedID(pick["hero_id"])]
+    if hero['attack_type'] == 'Melee':
+        features.append(1)
+    else:
+        features.append(0)
+    if hero['primary_attr'] == 'str':
+        features += [1, 0, 0]
+    elif hero['primary_attr'] == 'agi':
+        features += [0, 1, 0]
+    else:
+        features += [0, 0, 1]
+    features += [1 if ROLES[t] in hero['roles'] else 0 for t in range(9)]
+    return features
 
-print "reading training data.."
-trials = getData("data-41705/train-40705.data")
+def getData(filename, winOnly=True, xFeatures=True, yFeatures=False):
+    # raw is a list with all the games. each game is a dict.
+    raw = pickle.load(open(filename, "rb"))
+    # won is a bool. true only when the team with the last pick won.
+    won = lambda game: (game["picks_bans"][-1]["team"] == 0 and game["radiant_win"]) or (game["picks_bans"][-1]["team"] == 1 and not game["radiant_win"])
 
-# None allows us to feed in variable-length inputs
+    X = [(flatten(map(lambda hero: getOneHot(hero) + (getFeatures(hero) if xFeatures else []), game["picks_bans"][:-1])) + [game["picks_bans"][0]["team"]] + [] if winOnly else [
+        won(game)], getOneHot(game["picks_bans"][-1]) + (getFeatures(game["picks_bans"][-1]) if yFeatures else [])) for game in raw if
+         len(game["picks_bans"]) == 20 and (not winOnly or won(game))]
+    return X
+
+print("reading training data..")
+trials = getData("matches-100000.data")
+
+# we create placeholders where our data will go. None allows us to feed in variable-length inputs.
 x = tf.placeholder(tf.float32, shape=[None, len(trials[0][0])])
-y_ = tf.placeholder(tf.float32, shape=[None, N])
+y_ = tf.placeholder(tf.float32, shape=[None, len(trials[0][1])])
 
-W_1 = tf.Variable(
-    tf.random_uniform([len(trials[0][0]), M],-1.0, 1.0))
-b_1 = tf.Variable(tf.zeros([1, M]))
 
-h_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, W_1), b_1))
+# we create the set of initial weights and biases.
+# the dimensions are so that (x*W_1 + b_1 )*W_2+b_2 has the same shape as y_
+W_1 = tf.Variable(tf.random_uniform([len(trials[0][0]), M], -1.0, 1.0), name='Weights1')
+b_1 = tf.Variable(tf.zeros([1, M]), name='Bias1')
+W_2 = tf.Variable(tf.random_uniform([M, len(trials[0][1])], -1.0, 1.0), name='Weights2')
+b_2 = tf.Variable(tf.zeros([1, len(trials[0][1])]), name='Bias2')
 
-W_2 = tf.Variable(
-    tf.random_uniform([M, N],-1.0, 1.0))
-b_2 = tf.Variable(tf.zeros([1, N]))
+# we create our model.
+def model(W_1, b_1, W_2,b_2):
+    h_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, W_1), b_1),name='Output1')
+    y0 = tf.matmul(h_1, W_2) + b_2
+    y = tf.nn.softmax(y0, name='Output2-normalized')
+    return y0, y
 
-# h_2 = tf.nn.sigmoid(tf.add(tf.matmul(h_1, W_2), b_2))
+y0, y = model(W_1, b_1, W_2, b_2)
 
-# W_3 = tf.Variable(
-#     tf.random_uniform([L, N],-1.0, 1.0))
-# b_3 = tf.Variable(tf.zeros([1, N]))
-
-y0 = tf.matmul(h_1, W_2) + b_2
-y = tf.nn.softmax(y0)
-
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = y0, labels = y_)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y0, labels=y_, name='cross_entropy')
 train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
-
 init = tf.global_variables_initializer()
+
 with tf.Session() as sess:
-	print "starting training.."
-	sess.run(init)
-	for step in range(len(trials) / BATCH_SIZE):
-	  batch_xs, batch_ys = zip(*random.sample(trials, BATCH_SIZE))
-	  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    # create a graph that can be viewed on tensorboard.
+    writer = tf.summary.FileWriter(logdir="/tmp/tf1", graph=sess.graph)  # run python -m tensorflow.tensorboard --logdir=
+    merged = tf.summary.merge_all(key='summaries')
 
-	 #TODO: train batches
-	 #TODO: more layers
-	 #TODO: try backwards architecture
+    #initialize the variables
+    sess.run(init)
 
-	print "reading test data.."
-	tests = getData("data-41705/test-1000.data")
+    # start the training.
+    # since we have len(trials) many test points, and we are feeding it
+    # BATCH_SIZE many points at the time we will update our weights and biases a total of len(trials)/BATCH_SIZE
+    print("starting training..")
+    for step in range(len(trials) // BATCH_SIZE):
+        batch_xs, batch_ys = zip(*random.sample(trials, BATCH_SIZE))
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # TODO: train batches
+        # TODO: more layers
+        # TODO: try backwards architecture
 
-	sum = 0
-	for testx, testy in tests:
+    print("reading test data..")
 
-		values = sess.run(y, feed_dict={x: [testx], y_: [testy]})
-		predicted = np.argmax(values, 1)
-		actual = np.argmax(testy, 0)
+    tests = getData("matches-100000.data")
 
-		if actual == predicted[0]: sum += 1
+    # checks if the prediction is correct.
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))    # its a boolean
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  # cast converts the bool into a float
 
-		# print predicted, actual, values[0][actual]
+    sum = 0
+    for testx, testy in tests:
 
-		h = []
-		for i in range(0, len(testx) - N, N):
-			h.append(vec2hero(testx[i:i + N])),
+        values = sess.run(y, feed_dict={x: [testx], y_: [testy]})   # its a numpy.ndarray
+        predicted = np.argmax(values, 1)
+        actual = np.argmax(testy, 0)
 
-		teams = [Team("picking team"), Team("other team")]
-		for i in range(len(PICK_BAN_ORDER)):
-			team = teams[PICK_BAN_ORDER[i][1]]
-			if PICK_BAN_ORDER[i][0]:
-				team.pick(h[i])
-			else:
-				team.ban(h[i])
+        if actual == predicted[0]: sum += 1
 
-		print teams[0]
-		print teams[1]
+        # print predicted, actual, values[0][actual]
 
-		print "predicted:", getName(int2hero(predicted))
-		print "actual:", getName(int2hero(actual))
-		print "-----------------------------"
+        h = []
+        for i in range(0, len(testx) - N, N):
+            h.append(vec2hero(testx[i:i + N])),
 
-	print sum, "/", len(tests)
+        teams = [Team("picking team"), Team("other team")]
+        for i in range(len(PICK_BAN_ORDER)):
+            team = teams[PICK_BAN_ORDER[i][1]]
+            if PICK_BAN_ORDER[i][0]:
+                team.pick(h[i])
+            else:
+                team.ban(h[i])
 
-	X, Y = zip(*tests)
-	print(sess.run(accuracy, feed_dict={x: X, y_: Y}))
+        #print(teams[0])
+        #print(teams[1])
+
+        print("predicted:", getName(int2hero(predicted)))
+        print("actual:", getName(int2hero(actual)))
+        print("-----------------------------")
+
+    print(sum, "/", len(tests))
+
+    X, Y = zip(*tests)
+    print(sess.run(accuracy, feed_dict={x: X, y_: Y}))
