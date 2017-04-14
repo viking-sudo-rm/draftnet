6dd1c3b5b5d0aed4be1a6cecd5ec8231ab16945b
viking-sudo-rm <vikingarnir.will@gmail.com>
2017-04-14 10:44:10 -0400

    renamed file for embeddings

--- download.py
+++ download.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
-import requests, json, time, random
+import requests, json, time, random, argparse
 
-N = 9000
+argparser = argparse.ArgumentParser(description="Set train and test files.")
+argparser.add_argument('--N', help='number of games to download', type=int, default=50000)
+args = argparser.parse_args()
+
 WAIT_TIME = 60
 
 
@@ -12,7 +15,7 @@ if __name__ == "__main__":
 
 	matches = []
 
-	for i in range(0, N, 100):
+	for i in range(0, args.N, 100):
 	    res = get("https://api.opendota.com/api/explorer?sql=select%20match_id,picks_bans,radiant_win%20from%20matches%20where%20game_mode=2%20and%20picks_bans%20is%20not%20null%20order%20by%20match_id%20desc%20limit%20100%20offset%20" + str(i))
 
 	    if "error" in res.keys():
@@ -21,7 +24,7 @@ if __name__ == "__main__":
 	        continue
 
 	    matches += res["rows"]
-	    print("got", i + 100, "/", N, "matches")
+	    print("got", i + 100, "/", args.N, "matches")
 
 	print("checking downloaded game data..")
 	print("# with drafts:", sum(map(lambda x: 0 if x["picks_bans"] == None else 1, matches)))
@@ -33,8 +36,10 @@ if __name__ == "__main__":
 
 	print("sampling test and train datasets..")
 	random.shuffle(matches)
-	test = matches[:N // 10]
-	train = matches[N // 10:]
+	test = matches[:args.N // 10]
+	train = matches[args.N // 10:]
+
+	# TODO determine size of test set as a constant; not as a proportion
 
 	print("saving train dataset..")
 	filename = "data/train-" + str(len(train)) + ".json"
