c0a14db7e0fbd482a69d52a50cad77c1885c4a96
William Merrill <vikingarnir.will@gmail.com>
2017-05-01 18:57:20 -0400

    reconfigured django installation

--- draftsite/draftsite/wsgi.py
+++ draftsite/draftsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for public project.
+WSGI config for draftsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "public.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "draftsite.settings")
 
 application = get_wsgi_application()
--- site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from hello.views import HomePageView
-
-urlpatterns = patterns(
-    '',
-
-    url(r'^$', HomePageView.as_view(), name='home'),
-)
